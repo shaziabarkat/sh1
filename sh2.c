ef9e1a971d0db55504e0c8bec481d8f34c4baf7e
shaziabarkat <35466531+shaziabarkat@users.noreply.github.com>
2018-01-19 01:27:12 +0530

    Add files via upload

---
+++ sh2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,i;
+	printf("enter the no of times");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("Hello");
+		printf("\n");
+	}
+	getch();
+	
